813a41d52d889d7b8ce80ab659e9a0962dfa5277
doniatung <doniatung32@gmail.com>
2017-02-13 19:44:37 -0500

    algo explanation done :)

--- MergeSortTester.java
+++ MergeSortTester.java
@@ -1,19 +1,26 @@
+//PowerPuff Girls (Connie Lei, Yedoh Kang, Donia Tugn)
+//APCS2 pd1
+//HW07 -- What Does the Data Say?
+//2017-02-13
+
+
+
 /*======================================
   class MergeSortTester
 
   ALGORITHM:
-  <INSERT YOUR DISTILLATION OF ALGO HERE>
+  The algorithm works using recursion and a helper method entitled merge that takes two sorted lists and merges them. The recursion in sort checks if the inputted array is less than or equal to 1. If so, it returns that array, and if not, it creates two lists of roughly half the length of the original array. Then, merge is called on the "sort" of the two halfs of the original list.
 
   BIG-OH CLASSIFICATION OF ALGORITHM:
   <INSERT YOUR EXECUTION TIME CATEGORIZATION OF MERGESORT HERE>
 
   Mean execution times for dataset of size n:
   Batch size: <# of times each dataset size was run>
-  n=1       time: 
-  n=10      time: 
-  n=100     time: 
+  n=1       time: 1 
+  n=10      time: 10x10
+  n=100     time: 100x100
   ...
-  n=<huge>  time: 
+  n=<huge>  time: hugexhuge
 
   ANALYSIS:
   <INSERT YOUR RESULTS ANALYSIS HERE>
